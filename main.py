f226d2822e8ee9ea43368e0764fbdc267c379b3a
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-10-05 15:19:57 -0400

    Update times-tables function

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import os
 
 def menu():
     print("""Mental Math (Double-M) \n(A) Addition\n(S) Subtraction\n(M) Multiplication\n(D) Division\n(T) Times-tables """)
-    menu_input = input("User Input: ")
+    menu_input = input("Input: ")
 
     while menu_input != "T":
         menu_input = input("Select a valid input:")
 
-        if menu_input == "T":
-            times_tables()
+    if menu_input == "T":
+        times_tables()
 
 def times_tables():
-    os.system('cls')
-    print("Times-tables\nPress Q to quit")
 
-    
+    for left_operator in range (2, 13):
+        for right_operator in range(2, 13):
+            answer = str(left_operator * right_operator)
+            os.system('cls')
+            print("Times-tables\nPress Q to quit\n")
+
+            while True:
+                tt_input = input(f"{left_operator} * {right_operator} = ")
+
+                if tt_input == answer:
+                    break
+                elif tt_input == "Q":
+                    return
+                else:
+                    os.system('cls')
+                    print("Times-tables\nPress Q to quit\n")
+
+
+            
 
 
 if __name__ == '__main__':
