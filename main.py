17b0b811eafefb6da489ae9b5b8a39ba23255314
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-10-09 17:05:48 -0400

    Add lowercase input validation

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def menu():
         while True:
             
             range_input = input("Enter left operand range as a,b: ")
-            if range_input == 'Q':
+            if range_input == 'Q'or range_input == 'q':
                 exit() 
-            elif range_input == "B":
+            elif range_input == "B" or range_input == 'b':
                 menu()
             try:
                 range_a, range_b = map(int, range_input.split(","))
@@ -21,9 +21,9 @@ def menu():
                 continue
 
             range_input = input("Enter right operand range as c,d: ")
-            if range_input == 'Q':
+            if range_input == 'Q' or range_input == 'q':
                 exit() 
-            elif range_input == "B":
+            elif range_input == "B" or range_input == 'b':
                 menu()
             try:
                 range_c, range_d = map(int, range_input.split(","))
@@ -31,10 +31,10 @@ def menu():
                 print("Invalid input. Please enter a valid range as c,d or 'Q' to quit.")
                 continue
 
-            num_questions = input("Number of questions: ")
+            num_questions = input("Number of questions: ").upper()
             if num_questions == 'Q':
                 exit()  
-            elif range_input == "B":
+            elif num_questions == "B":
                 menu()
             try:
                 num_questions = int(num_questions)
@@ -45,10 +45,12 @@ def menu():
             if range_a > range_b or range_c > range_d or num_questions < 0:
                 input("Invalid response. Press enter and retype your inputs. ")
                 question_range(operation_sign)
-            range_input = input(f"Range - [{range_a} to {range_b}] {operation_sign} [{range_c} to {range_d}] for {num_questions} questions (Y/N): ")
-            if range_input == 'Q':
+
+            range_input = input(f"Range - [{range_a} to {range_b}] {operation_sign} [{range_c} to {range_d}] for {num_questions} questions (Y/N): ").upper
+            
+            if range_input == 'Q' or range_input == 'q':
                 exit() 
-            elif range_input == "B":
+            elif range_input == "B" or range_input == 'b':
                 menu()
             try:
                 num_questions = int(num_questions)
@@ -56,7 +58,7 @@ def menu():
                 print("Invalid input. Please enter a valid response.")
                 continue
 
-            if range_input == "Y":
+            if range_input == "Y" or range_input == 'y':
                 if operation_sign == "+":
                     addition(range_a, range_b, range_c, range_d, operation_sign, num_questions)
                 elif operation_sign == "-":
@@ -65,20 +67,20 @@ def menu():
                     multiplication(range_a, range_b, range_c, range_d, operation_sign, num_questions)  
                 elif operation_sign == "/":
                     division(range_a, range_b, range_c, range_d, operation_sign, num_questions)
-            if range_input == "N":
+            if range_input == "N" or range_input == 'n':
                 question_range(operation_sign) 
-            if range_input == "Q":
+            if range_input == "Q" or range_input == 'q':
                 exit()
-            if range_input == "B":
+            if range_input == "B" or range_input == 'b':
                 menu()
     
     def help_menu():
         os.system('cls')
         print("Mental Math Monster (Triple-M-Lite)\nPress 'Q' to quit the game and 'B' to go back to the main menu.\n")
-        help_menu_input = input("Input: ")
+        help_menu_input = input("Input: ").upper()
         help_menu_list = ["Q", "B"]
         while help_menu_input not in help_menu_list:
-            menu_input = input("Select a valid input:")
+            menu_input = input("Select a valid input:").upper()
         if help_menu_input == "Q":
             exit()
         if help_menu_input == "B":
@@ -87,7 +89,7 @@ def menu():
     menu_list = ["A", "S", "M", "D", "Q", "H"]
     os.system("cls")
     print("""Mental Math Monster (Triple-M-Lite) \n(A) Addition\n(S) Subtraction\n(M) Multiplication\n(D) Division\n(H) Help\n """)
-    menu_input = input("Input: ")
+    menu_input = input("Input: ").upper()
     while menu_input not in menu_list:
         menu_input = input("Select a valid input:")
     if menu_input == "A":
@@ -119,8 +121,10 @@ def addition(range_a, range_b, range_c, range_d, operation_sign, num_questions):
 
             if user_input == answer:
                 break
-            elif user_input == "Q":
+            elif user_input == "Q" or user_input == 'q':
                 exit()
+            elif user_input == "B" or user_input == 'b':
+                menu()
             else:
                 os.system('cls')
                 print("Addition\nPress Q to quit\n")
@@ -139,8 +143,10 @@ def subtraction(range_a, range_b, range_c, range_d, operation_sign, num_question
 
             if user_input == answer:
                 break
-            elif user_input == "Q":
+            elif user_input == "Q" or user_input == 'q':
                 exit()
+            elif user_input == "B" or user_input == 'b':
+                menu()
             else:
                 os.system('cls')
                 print("Subtraction\nPress Q to quit\n")
@@ -159,8 +165,10 @@ def multiplication(range_a, range_b, range_c, range_d, operation_sign, num_quest
 
             if user_input == answer:
                 break
-            elif user_input == "Q":
+            elif user_input == "Q" or user_input == 'q':
                 exit()
+            elif user_input == "B" or user_input == 'b':
+                menu()
             else:
                 os.system('cls')
                 print("Multiplication\nPress Q to quit\n")
@@ -179,8 +187,10 @@ def division(range_a, range_b, range_c, range_d, operation_sign, num_questions):
 
             if user_input == answer:
                 break
-            elif user_input == "Q":
+            elif user_input == "Q" or user_input == 'q':
                 exit()
+            elif user_input == "B" or user_input == 'b':
+                menu()
             else:
                 os.system('cls')
                 print("Division\nPress Q to quit\n")
